7d8d90c8851055e966228ca060f3fed048e6a2fd
wengzhifang <wengzhifang@yonglibao.com>
2016-04-23 14:50:24 +0800

    回滚米迪

--- CashMaker/TaskRecordsModel.m
+++ CashMaker/TaskRecordsModel.m
@@ -14,10 +14,9 @@
 {
     self.taskRecordsID = dic[@"id"];
     self.source_type = dic[@"source_type"];
-    self.source_name = @" ";
     self.change_coins = dic[@"change_coins"];
 
-//    self.created_at = dic[@"created_at"];
-    self.created_at = @" ";
+    self.created_at = dic[@"created_at"];
+    
 }
 @end
